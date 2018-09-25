133296167cc0c4b34c33b5c721a1cc68e8abc88f
xiao0811 <panxiao0811@gmail.com>
2018-09-25 10:37:53 +0800

    500

---
+++ classic/less04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, i;
+    for (i = 0;i < 10; i++) {
+        if (100 * i + 10 * i + i + 10 * i + i + 3 * i == 500) {
+            num = i;
+            printf("num is %d.\n", num);
+        }
+    }
+    return 0;
+}
