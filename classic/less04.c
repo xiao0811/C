d6035794c932968de52d3f8a5e755d7ae5673c44
xiao0811 <panxiao0811@gmail.com>
2018-09-26 11:17:18 +0800

    change less04

--- classic/less04.c
+++ classic/less04.c
@@ -4,7 +4,14 @@ int main()
 {
     int num, i;
     for (i = 0;i < 10; i++) {
+        /*
         if (100 * i + 10 * i + i + 10 * i + i + 3 * i == 500) {
+            num = i;
+            printf("num is %d.\n", num);
+        } 
+        */
+
+        if (i < 10) {
             num = i;
             printf("num is %d.\n", num);
         }
